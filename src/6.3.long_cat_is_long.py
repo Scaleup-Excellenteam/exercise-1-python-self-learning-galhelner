3a393f97547bd48c59b9afca003a979b400097a8
galhelner <galhelner@gmail.com>
2025-03-30 22:11:18 +0300

    Update 6.3.long_cat_is_long.py

--- src/6.3.long_cat_is_long.py
+++ src/6.3.long_cat_is_long.py
@@ -1,6 +1,8 @@
 """
 Week 6 Ex 3 - long_cat_is_long
 """
+
+
 def long_cat_is_long(text):
     """Create a dictionary where the keys are length of valid alpha words and values are the words.
 
@@ -11,18 +13,14 @@ def long_cat_is_long(text):
         dict: A dictionary where the keys are length of valid alpha words and values are the words.
     """
     # split the text into words and remove any symbol that is not an alpha
-    words = [''.join(filter(str.isalpha, word.lower())) for word in text.split()]
+    split_words = [''.join(filter(str.isalpha, word.lower())) for word in text.split()]
+    # remove empty words added because join
+    words = [word for word in split_words if word]
     # build a dictionary where keys are the words and values are the word's length
     return {word: len(word) for word in words}
 
 
 if __name__ == '__main__':
     # test case
-    INPUT_TEXT = """
-You see, wire telegraph is a kind of a very, very long cat.
-You pull his tail in New York and his head is meowing in Los Angeles.
-Do you understand this?
-And radio operates exactly the same way: you send signals here, they receive them there.
-The only difference is that there is no cat.
-    """
-    print(long_cat_is_long(INPUT_TEXT))
+    result = long_cat_is_long("Bla bla bla tralala 123")
+    print(result)
