31289590339e03f6291edc935f35149a3a4adfad
galhelner <galhelner@gmail.com>
2025-03-30 21:55:17 +0300

    Update 5.3.parsle_tongue.py

--- src/5.3.parsle_tongue.py
+++ src/5.3.parsle_tongue.py
@@ -1,6 +1,8 @@
 """
 Week 5 Ex 3 - parsle_tongue
 """
+
+
 def is_lower_alpha(byte):
     """Check if a binary byte is an ascii code of a lower case english letter.
 
@@ -13,7 +15,7 @@ def is_lower_alpha(byte):
     return 97 <= byte <= 122
 
 
-def parsle_tongue(filepath='resources/logo.jpg', chunk_size=1024):
+def parsle_tongue(filepath='../resources/logo.jpg', chunk_size=1024):
     """Yields an encrypted messages from a binary image file.
 
     Args:
