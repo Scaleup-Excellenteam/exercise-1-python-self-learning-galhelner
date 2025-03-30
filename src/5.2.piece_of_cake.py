42b0f1ddda9374aa8a69a8b1e48dd1ab88cf89dd
galhelner <galhelner@gmail.com>
2025-03-30 20:02:16 +0300

    Update 5.2.piece_of_cake.py

--- src/5.2.piece_of_cake.py
+++ src/5.2.piece_of_cake.py
@@ -1,4 +1,20 @@
+"""
+Week 5 Ex 2 - piece_of_cake
+"""
 def piece_of_cake(prices, optionals=None, **amounts):
+    """Calculate the total price of a cake recipe based on the ingredients prices per 100g and amonts.
+
+    Args:
+        prices (dict): A dictionary of the ingredients prices, key is the ingredient name and value is the price per 100g.
+        optionals (list, optional): list of optional ingredients for the cake recipe (we want to save money so they doesn't count in the total price).
+        amounts (dict): A dictionary of the ingredients amounts, key is the ingredient name and value is the amount in grams.
+
+    Returns:
+        int: The total price that we have to pay for all the cake ingredients.
+
+    Raises:
+        KeyError: If ingredient key from the prices dict is not exist as a key in the amounts dict.
+    """
     if optionals is None:
         # no optional ingredients
         optionals = []
