26c4188c028157847ef12b33db0cf1c78c35ec67
galhelner <galhelner@gmail.com>
2025-03-30 19:36:51 +0300

    Update 5.1.no_vinnigrete.py

--- src/5.1.no_vinnigrete.py
+++ src/5.1.no_vinnigrete.py
@@ -102,10 +102,10 @@ def get_user_dates():
 
 if __name__ == '__main__':
     # get the start and end dates from the user
-    start_date, end_date = get_user_dates()
+    starting_date, ending_date = get_user_dates()
 
     # get a random date in the range between them
-    random_date = get_random_date(start_date, end_date)
+    random_date = get_random_date(starting_date, ending_date)
 
     # print the random date
     print("Random date:", random_date.date())
