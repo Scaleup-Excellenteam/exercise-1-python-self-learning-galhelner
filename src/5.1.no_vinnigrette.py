b70fa378b134215614473692925494b9adc7cb5e
galhelner <galhelner@gmail.com>
2025-03-30 21:42:30 +0300

    Update 5.1.no_vinnigrette.py

--- src/5.1.no_vinnigrette.py
+++ src/5.1.no_vinnigrette.py
@@ -112,7 +112,7 @@ def no_vinnigrete(starting_date, ending_date):
     ending_date_obj = convert_date(ending_date)
     random_date = get_random_date(starting_date_obj, ending_date_obj)
     if is_monday(random_date):
-        print("Ain't gettin' no vinaigrette today: " + str(random_date))
+        print("Ain't gettin' no vinaigrette today :(")
     else:
         print(random_date)
 
