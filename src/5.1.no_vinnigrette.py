267bed4bf3dda31a83950215f81e36510932a441
galhelner <galhelner@gmail.com>
2025-03-30 21:16:49 +0300

    Update 5.1.no_vinnigrette.py

--- src/5.1.no_vinnigrette.py
+++ src/5.1.no_vinnigrette.py
@@ -103,7 +103,10 @@ def get_user_dates():
 def no_vinnigrete(starting_date, ending_date):
     starting_date_obj = convert_date(starting_date)
     ending_date_obj = convert_date(ending_date)
-    return get_random_date(starting_date_obj, ending_date_obj)
+    random_date = get_random_date(starting_date_obj, ending_date_obj)
+    print(random_date)
+    if is_monday(random_date):
+        print("Ain't gettin' no vinaigrette today")
 
 
 if __name__ == '__main__':
