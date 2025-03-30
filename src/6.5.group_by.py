9ac4d8d0a7d626a0d17396467e0baed44e871e4d
galhelner <galhelner@gmail.com>
2025-03-30 20:36:05 +0300

    Update 6.5.group_by.py

--- src/6.5.group_by.py
+++ src/6.5.group_by.py
@@ -1,7 +1,18 @@
-import itertools
-
-
+"""
+Week 6 Ex 5 - group_by
+"""
 def group_by(func, iterable):
+    """Create a dictionary where keys are the return value of the function on the iterable items,
+        and values are a list of all items with the same return value.
+
+        Args:
+            func (function): Function to call with each iterable item as argument.
+            iterable (iterable): Iterable of arguments items.
+
+        Returns:
+            dict: A dictionary where keys are the return value of the function on the iterable items,
+             and values are a list of all items with the same return value.
+    """
     try:
         # using dictionary comprehension to create the final dictionary
         # and a list comprehension inside of it to create for each key the values list
