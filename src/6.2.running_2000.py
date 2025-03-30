ca6ac6d17f09a94b9ce5055489212d433f3ddfa2
galhelner <galhelner@gmail.com>
2025-03-30 20:21:10 +0300

    Update 6.2.running_2000.py

--- src/6.2.running_2000.py
+++ src/6.2.running_2000.py
@@ -1,14 +1,19 @@
+"""
+Week 6 Ex 2 - running_2000
+"""
 import time
 
 
 def running_2000(f, *args, **kwargs):
-    """
-    Measure the running time of a function with its arguments
-    Using pref_counter for higher precision
-    :param f: function to measure
-    :param args: arguments to pass to the function
-    :param kwargs: key-value pairs arguments to pass to the function
-    :return: the running time of the function execution
+    """Measure the running time of a function with its arguments Using pref_counter for higher precision.
+
+    Args:
+        f (function): function to measure.
+        args (tuple): arguments to pass to the function.
+        kwargs (dictionary): key-value pairs arguments to pass to the function.
+
+    Returns:
+        float: the running time of the function execution.
     """
     start_time = time.perf_counter()  # store starting time
     f(*args, **kwargs)  # call the function with given arguments
@@ -20,4 +25,3 @@ if __name__ == '__main__':
     # test cases
     print(running_2000(print, 'hello'))
     print(running_2000(zip, [1, 2, 3], [4, 5, 6]))
-    print(running_2000("Hi {name}".format, name="Bug"))
